f4cb86bbf590a809a8f58891e9386134264ca41f
marianoPersonal <mariano_farace@hotmail.com>
2023-02-05 11:58:12 +0100

    First commit

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,15 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styleUrls: ['./busqueda.component.css'],
+})
+export class BusquedaComponent {
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+
+    this.txtBuscar.nativeElement.value = '';
+  }
+}
